08981cbe36e434a6d1499d13e4edb6c2f3c3473f
Sanky2104 <guptasanskriti21@gmail.com>
2022-03-12 01:36:28 +0530

    Updates

--- trials/linked_list.c
+++ trials/linked_list.c
@@ -6,235 +6,222 @@
 struct Node 
 {
 	int data;
-	struct Node *next;
+	struct Node *link;
 };
-void display(struct Node *start) //printing the list 
+struct Node *start=NULL;
+void display();
+struct Node *create_node();
+void ins_beg(int item);
+void ins_end(int item);
+void ins_pos(int item, int pos);
+int del_beg();
+int del_end();
+int del_pos(int pos);
+void main()
 {
-    printf("Current List Is:\n");
-	while(start != NULL)
-	{
-		printf("%d ",start->data);
-		start = start->next;
-	}
-	printf("\n");
-} 
-//using pointer to pointer as call by refrence
-void insert_begin(struct Node **start, int item) //inserting element in the begining 
-{
-    struct Node *new;
-    new=(struct Node *)malloc(sizeof(struct Node));
-    if(new==NULL)
+    int item,pos,choice;
+    while(1)
     {
-        printf("Empty list \n");
-        return;
+        printf("\n1. Insert at bginning");
+        printf("\n2. Insert at end");
+        printf("\n3. Insert at position");
+        printf("\n4. Delete at beginning");
+        printf("\n5. Delete at end");
+        printf("\n6. Delete at position");
+        printf("\n7. Display");
+        printf("\n8. Exit\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            printf("\nEnter the element: ");
+            scanf("%d",&item);
+            ins_beg(item);
+            break;
+            case 2:
+            printf("\nEnter the element: ");
+            scanf("%d",&item);
+            ins_end(item);
+            break;
+            case 3:
+            printf("\nEnter the element: ");
+            scanf("%d",&item);
+            printf("\nEnter the position: ");
+            scanf("%d",&pos);
+            ins_pos(item,pos);
+            break;
+            case 4:
+            item=del_beg();
+            printf("\nDeleted Item: %d",item);
+            break;
+            case 5:
+            item=del_end();
+            printf("\nDeleted Item: %d",item);
+            break;
+            case 6:
+            printf("\nEnter the position: ");
+            scanf("%d",&pos);
+            item=del_pos(pos);
+            printf("\nDeleted Item: %d",item);
+            break;
+            case 7:
+            display();
+            break;
+            case 8:
+            exit(0);
+            default:
+            printf("\nWrong Choice");
+        }
     }
+}
+struct Node *create_node()
+{
+    struct Node *temp=(struct Node *)malloc(sizeof(struct Node));
+    return temp;
+}
+void ins_beg(int item)
+{
+    struct Node *new=create_node();
     new->data=item;
-    if(*start==NULL)
+    if(start==NULL)
     {
-        *start=new;
+        start=new;
+        new->link=NULL;
     }
     else
     {
-        new->next=*start;
-        *start=new;
+        new->link=start;
+        start=new;
     }
 }
-void insert_end(struct Node **start, int item)//inserting at the end
+void ins_end(int item)
 {
-    struct Node* new,*ptr;
-    new= (struct Node *)malloc(sizeof(struct Node));
-    if(new==NULL)
+    struct Node *new=create_node();
+    new->data=item;
+    new->link=NULL;
+    if(start==NULL)
     {
-        printf("No memory space \n");
-        return;
-    }
-    new->next=NULL;
-    if(*start==NULL)
-    {
-        *start==new;
+        start=new;
     }
     else
     {
-      ptr=*start;
-      while(ptr->next!=NULL)
-      {
-          ptr=ptr->next;
-      }
-    ptr->next=new;
-    return;
+        struct Node *ptr=start;
+        while(ptr->link!=NULL)
+        {
+            ptr=ptr->link;
+        }
+        ptr->link=new;
     }
 }
-void insert_pos(struct Node **start, int item, int pos)//inserting at a position
+void ins_pos(int item, int pos)
 {
-    struct Node *new,*ptr;
-    new=(struct Node *)malloc(sizeof(struct Node));
-    int i;
-    if(new==NULL)
-    {
-        printf("No memory space \n");
-        return;
-    }
+    struct Node *new=create_node();
+    struct Node *ptr=start;
     new->data=item;
-    new->next=NULL;
     if(pos==0)
     {
-        new->next=*start;
-        *start=new;
+        new->link=start;
+        start=new;
     }
     else
     {
-        for(i=0;i<pos;i++)
+        for(int i=0;i<pos;i++)
         {
-            ptr=ptr->next;
+            ptr=ptr->link;
             if(ptr==NULL)
             {
-                printf("Position not found \n");
-                return;
+                printf("\nPosition not found");
             }
         }
-        new->next=ptr->next;
-        ptr->next=new;
+        new->link=ptr->link;
+        ptr->link=new;
     }
 }
-int delete_begin(struct Node **start)//deleting from the begining
-{ int d;
+int del_beg()
+{
+    int d;
     struct Node *del;
-    if(*start==NULL)
+    if(start==NULL)
     {
-        printf("The list is empty \n");
+        printf("\nUnderflow");
         return 0;
     }
     else
     {
-        del=*start;
-        *start=(*start)->next;
-        d= del->data;
+        del=start;
+        start=start->link;
+        d=del->data;
         free(del);
     }
-    return(d);
+    return d;
 }
-int delete_end(struct Node **start)//deleting from the end
-{ int d;
-    struct Node *ptr,*temp;
-    if(*start==NULL)
+int del_end()
+{
+    int d;
+    struct Node *ptr,*ptr1;
+    if(start==NULL)
     {
-        printf("The list is empty \n");
+        printf("\nUnderflow");
         return 0;
     }
     else
     {
-        ptr=*start;
-        while(ptr->next !=NULL)
+        ptr=start;
+        while(ptr->link!=NULL)
         {
-            temp=ptr;
-            ptr=ptr->next;
+            ptr1=ptr;
+            ptr=ptr->link;
         }
-        temp->next=NULL;
+        ptr1->link=NULL;
         d=ptr->data;
         free(ptr);
-        return(d);
+        return d;
     }
 }
-int delete_pos(struct Node **start, int pos)// deleting from a given position
+int del_pos(int pos)
 {
     struct Node *temp,*ptr;
     int i,d;
-    if(*start==NULL)
+    if(start==NULL)
     {
-        printf("The list is empty \n");
+        printf("\nUnderflow");
         return 0;
     }
     else
     {
         if(pos==0)
         {
-            ptr=*start;
-            *start=(*start)->next;
+            ptr=start;
+            start=start->link;
             d=ptr->data;
             free(ptr);
-            return (d);
+            return d;
         }
         else
         {
-            ptr=*start;
+            ptr=start;
             for(i=0;i<pos;i++)
             {
                 temp=ptr;
-                ptr=ptr->next;
+                ptr=ptr->link;
                 if(ptr==NULL)
                 {
-                    printf("Position not found \n");
+                    printf("\nPosition not found");
                     return 0;
                 }
             }
-            temp->next=ptr->next;
+            temp->link=ptr->link;
             d=ptr->data;
             free(ptr);
-            return(d);
-            }
+            return d;
         }
     }
-
-int main()
+}
+void display()
 {
-	struct Node *Head=NULL;
-	int choice, item, deleted_item, pos;
-	while(1){
-	printf("\n1. To Insert at beginning");
-	printf("\n2. To insert at end");
-    printf("\n3. To insert at position");
-    printf("\n4. To delete at beginning");
-    printf("\n5. To delete at end");
-    printf("\n6. To delete at position");
-    printf("\n7. To Display");
-    printf("\n8. To exit\n");
-    scanf("%d",&choice);
-	switch(choice)
-	{
-		case 1:
-			printf("\nEnter the element: ");
-			scanf("%d", &item);
-			insert_begin(&Head, item);
-			break;
-
-		case 2:
-			printf("\nEnter the element: ");
-			scanf("%d", &item);
-			insert_end(&Head, item);
-			break;
-
-		case 3:
-			printf("\nEnter the element: ");
-			scanf("%d", &item);
-			printf("\nEnter the position: ");
-			scanf("%d", &pos);
-			insert_begin(&Head, item);
-			break;
-
-		case 4:
-			deleted_item = delete_begin(&Head);
-			printf("\nDeleted Item: %d",deleted_item);
-			break;
-
-		case 5:
-			delete_end(&Head);
-			printf("\nDeleted Item: %d",deleted_item);
-			break;
-
-		case 6:
-			printf("\nEnter position at which you want to delete\n");
-			scanf("%d", &pos);
-			delete_pos(&Head, pos);
-			printf("\nDeleted Item: %d\n",deleted_item);
-			break;
-
-		case 7:
-			display(Head);
-			break;
-		case 8:
-			exit(0);
-		default:
-			printf("\nWrong Choice");
-	}
-	}
+    while(start!=NULL)
+    {
+        printf("%d ",start->data);
+        start=start->link;
+    }
 }
