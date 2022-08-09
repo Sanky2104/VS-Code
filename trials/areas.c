9b2d1db555c15fbfedb70b33a270da9da5eb671c
Sanky2104 <guptasanskriti21@gmail.com>
2022-08-09 12:07:16 +0530

    Updates

--- trials/areas.c
+++ trials/areas.c
@@ -19,22 +19,27 @@ void main()
                 printf("\nEnter the base and height of the parallelogram: ");
                 scanf("%f%f",&a,&b);
                 printf("\nArea of parallelogram: %f",a*b);
+                break;
             case 2:
                 printf("\nEnter the 1st base, 2nd base & the height of the trapezoid: ");
                 scanf("%f%f%f",&a,&b,&c);
                 printf("\nArea of trapezoid: %f",(a+b)*(c/2));
+                break;
             case 3:
                 printf("\nEnter the 1st and 2nd diagonal of the rhombus: ");
                 scanf("%f%f",&a,&b);
                 printf("\nArea of rhombus: %f",(a*b)/2);
+                break;
             case 4:
                 printf("\nEnter the radius of the sphere: ");
                 scanf("%f",&a);
                 printf("\nArea of sphere: %f",4*a*a*3.14);
+                break;
             case 5:
                 printf("\nEnter the 1st & 2nd axis of the ellipse: ");
                 scanf("%f%f",&a,&b);
                 printf("\nArea of the ellipse: %f",3.14*a*b);
+                break;
             case 6:
                 exit(0);
         }
