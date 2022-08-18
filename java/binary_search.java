853139aab79d8e54b6e1bd6f5405635a8f7b675f
Sanky2104 <guptasanskriti21@gmail.com>
2022-08-18 12:05:58 +0530

    Updates

--- java/binary_search.java
+++ java/binary_search.java
@@ -4,32 +4,38 @@ public class binary_search
     public static void main(String[] args)
     {
         Scanner var = new Scanner(System.in);
-        System.out.print("\nNumber of elements: ");
-        int n = var.nextInt();
-        int i;
-        int[] arr = new int[n];
-        for(i=0;i<n;i++)
+        try
         {
-            System.out.print("Enter a number: ");
-            arr[i] = var.nextInt();
+            System.out.print("\nNumber of elements: ");
+            int n = var.nextInt();
+            int i;
+            int[] arr = new int[n];
+            for(i=0;i<n;i++)
+            {
+                System.out.print("Enter a number: ");
+                arr[i] = var.nextInt();
+            }
+            System.out.print("\nNumber to be searched: ");
+            int key = var.nextInt();
+            System.out.print("\nArray: ");
+            for(i=0;i<n;i++)
+            {
+                System.out.print(arr[i]+ " ");
+            }
+            int result = b_search(arr, n, key);
+            if(result == 0)
+            {
+                System.out.print("\n" + key + " is NOT present");
+            }
+            else
+            {
+                System.out.print("\n" + key + " is present");
+            }
         }
-        System.out.print("\nNumber to be searched: ");
-        int key = var.nextInt();
-        System.out.print("\nArray: ");
-        for(i=0;i<n;i++)
+        finally
         {
-            System.out.print(arr[i]+ " ");
+            var.close();
         }
-        int result = b_search(arr, n, key);
-        if(result == 0)
-        {
-            System.out.print("\n" + key + " is NOT present");
-        }
-        else
-        {
-            System.out.print("\n" + key + " is present");
-        }
-        var.close();
     }
     public static int b_search(int[] arr, int n, int key)
     {
