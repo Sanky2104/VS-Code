0e4d17748e72cb0d834e4cf70924bdddb90fc0e6
Sanky2104 <guptasanskriti21@gmail.com>
2022-08-18 12:23:41 +0530

    Updates

--- java/sum_of_digits.java
+++ java/sum_of_digits.java
@@ -1,4 +1,8 @@
+
+//YE WALA GALAT HAI, NUMBER OF DIGITS SAHI HAI BUT SUM OF DIGITS GALAT HAI
+
 import java.util.Scanner;
+import java.lang.Math;
 public class sum_of_digits
 {
     public static void main(String[] args) 
@@ -8,17 +12,20 @@ public class sum_of_digits
         {
             System.out.print("\nEnter a number: ");
             int num = var.nextInt();
-            int i,count=0,sum=0, dig;
-            while(num!=0)
+            int original = num;
+            int i,count = 0 , sum = 0 , dig;
+            while(num != 0)
             {
                 count++;
                 num/=10;
             }
             System.out.println("Number of digits: " + count);
-            for(i=0; i<count; i++)
+            for(i = 0 ; i < count ; i++)
             {
-                dig = num % 10;
+                dig = (int)(original % (Math.pow(10,count-(i-1))));
+                sum = sum + dig;
             }
+            System.out.println("Sum of digits: " + sum);
         }
         finally
         {
