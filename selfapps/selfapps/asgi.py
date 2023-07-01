f74530b48ac01ad6aa68732772f9e2d7cace2f64
Sanky2104 <guptasanskriti21@gmail.com>
2023-07-01 11:13:21 +0530

    Updates

---
+++ selfapps/selfapps/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for selfapps project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'selfapps.settings')
+
+application = get_asgi_application()
